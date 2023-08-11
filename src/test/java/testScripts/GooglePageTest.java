4c32c0be93339426e1e9b85dc737e37d95864a0b
Ipssidhu <141715481+Ipssidhu@users.noreply.github.com>
2023-08-11 08:26:05 +0000

    Update GooglePageTest.java

--- src/test/java/testScripts/GooglePageTest.java
+++ src/test/java/testScripts/GooglePageTest.java
@@ -1,5 +1,7 @@
 package testScripts;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -8,23 +10,37 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GooglePageTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
 		// ChromeDriver driver1= new ChromeDriver();
-		driver.get("https://www.google.com/");
+		driver.navigate().to("https://www.google.com/");
 	//	WebElement srcBox= driver.findElement(By.id("APjFqB"));
 	//	WebElement srcBox= driver.findElement(By.name("q"));
 		WebElement srcBox= driver.findElement(By.className("gLFyf"));
-		srcBox.sendKeys("JAVA Tutorial");
-		srcBox.sendKeys(Keys.ENTER);
+		
+	/*	srcBox.sendKeys(Keys.ENTER);
 		String PageTitle = driver.getTitle();
 		System.out.println("Page Title   " + PageTitle );
 		driver.navigate().back();
-		// driver.navigate().forward();
+	    driver.navigate().forward();
 		System.out.println("Backward Page Title   " + driver.getTitle() );
-		 driver.close();
-		
+		 driver.close(); 
+		 */
+		srcBox.sendKeys("Java Tutorial");
+		Thread.sleep(2000);
+		List<WebElement> items = driver.findElements(By.xpath("(//ul[@class='G43f7e'])[1]//li//descendant::div[@class='wM6W7d']"));
+		System.out.println(" no.of items ...." + items.size());
+		for(int i=0; i<items.size(); i++)
+		{ 
+			System.out.println(items.get(i).getText());
+			System.out.println(items.get(i).getAttribute("role"));
+			if(items.get(i).getText().equalsIgnoreCase("Java Tutorial pdf")) {
+				// items.get(i).click();
+				items.get(i).submit();
+			break;
+			}
+		}
 	}
 
 }
