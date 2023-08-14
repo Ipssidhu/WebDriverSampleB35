ad7bd4626c6e061a184696d2688092b4e4b82c5f
Administrator <Administrator@172.31.6.234>
2023-08-14 07:48:38 +0000

    14 AUg 04

--- src/test/java/testScripts/AlertTest.java
+++ src/test/java/testScripts/AlertTest.java
@@ -11,10 +11,22 @@ public class AlertTest {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 	    driver.navigate().to("http://demo.seleniumeasy.com/javascript-alert-box-demo.html");
-	   driver.findElement(By.xpath("//button[@class='btn btn-default']")).click();
+	    driver.findElement(By.xpath("//button[@class='btn btn-default']")).click();
 	    Alert alert = driver.switchTo().alert();
 	    System.out.println(" From Simple Alert  "+ alert.getText());
 	    alert.accept();
-	  //  driver.findElement(By.xpath("(//button[@class='btn btn-default btn-lg'])[2]")).click();
+	    
+	    driver.findElement(By.xpath("(//button[@class='btn btn-default btn-lg'])[1]")).click();
+	    Alert confirm = driver.switchTo().alert();
+	    System.out.println(" From Simple Alert  "+ confirm.getText());
+	    confirm.dismiss();
+	    
+	   driver.findElement(By.xpath("(//button[@class='btn btn-default btn-lg'])[2]")).click();
+	   Alert prompt = driver.switchTo().alert();
+	   System.out.println("From Prompt alert " + prompt.getText());
+	   prompt.sendKeys("Welcome");
+	   prompt.accept();
+	   
+	   
 	}	    
 }
